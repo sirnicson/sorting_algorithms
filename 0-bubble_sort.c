a23fd05cbf423fbef5909380a0ee89d7a26c0bef
sirnicson <sirnicson.nicson@gmail.com>
2024-10-26 11:46:52 +0300

    Implemented task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,43 +1,30 @@
-#include <stdio.h>
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- *               using the Bubble sort algorithm.
- *
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * bubble_sort - sorts an array in ascending order
+ * @array: array of ints to sort
+ * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	unsigned int i, j;
+	int tmp;
 
-    if (array == NULL || size < 2)
-    {
-        return;
-    }
+	if (size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-
-                print_array(array, size); /* Print array after each swap */
-            }
-        }
-        if (swapped == 0)
-        {
-            break; /* Stop if no swaps occurred (array is sorted) */
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
-
