1bf0a1b9766f36e888564942adae3700b93602fa
sirnicson <sirnicson.nicson@gmail.com>
2024-10-27 18:44:13 +0300

    Implemented task 2

--- sort.h
+++ sort.h
@@ -1,31 +1,23 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stddef.h>
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+/* Data structure for a doubly linked list */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-
-/* Provided Prototype */
+/* Prototypes for print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-#endif /* SORT_H */
+/* Prototypes for sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
+#endif /* SORT_H */
