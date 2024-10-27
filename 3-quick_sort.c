2e854da3f5d51f500465cc7c93b51486d78bedfb
sirnicson <sirnicson.nicson@gmail.com>
2024-10-27 18:48:30 +0300

    Implemented task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ * partition - Partitions the array using the Lomuto partition scheme.
+ * @array: The array to partition.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
+ * @size: The size of the array (for printing).
+ * Return: The final partition index.
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low;
+    int j;
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] < pivot)
+        {
+            if (i != j)
+            {
+                swap(&array[i], &array[j]);
+                print_array(array, size);
+            }
+            i++;
+        }
+    }
+    if (i != high)
+    {
+        swap(&array[i], &array[high]);
+        print_array(array, size);
+    }
+    return (i);
+}
+
+/**
+ * quicksort - Recursive function to implement Quick Sort.
+ * @array: The array to sort.
+ * @low: Starting index.
+ * @high: Ending index.
+ * @size: Size of the array.
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        int pi = partition(array, low, high, size);
+        quicksort(array, low, pi - 1, size);
+        quicksort(array, pi + 1, high, size);
+    }
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick Sort.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    quicksort(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -19,5 +19,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
