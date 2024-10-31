e30647485e2d90da4550277d4f0d3ffebebaacc4
sirnicson <sirnicson.nicson@gmail.com>
2024-10-31 09:50:46 +0300

    updated task 1

--- sort.h
+++ sort.h
@@ -17,7 +17,7 @@ void print_list(const listint_t *list);
 
 /* Prototypes for sorting algorithms */
 void bubble_sort(int *array, size_t size);
-void insertion_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
